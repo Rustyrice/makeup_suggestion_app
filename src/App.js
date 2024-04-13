e2d3c6d033fe548b5566e617f0d20bd2f9cc1817
matangolani <matgolani@gmail.com>
2024-04-13 14:59:56 +0100

    Added eye tracker

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import webgazer from 'webgazer'
 
 function App() {
+  useEffect(() => {
+    window.saveDataAcrossSessions = true;
+
+    // Webgazer script
+    webgazer.setGazeListener((data, timeStamp) => {
+      console.log(data, timeStamp);
+    }).begin();
+
+    // Clean up function if necessary
+    return () => {
+      // Clean up code here if needed
+    };
+  }, []); // Empty dependency array to run effect only once
   const handleImportClick = () => {
     alert('Import cv...')
   }
